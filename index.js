c274b966e753331b51d8a0965d69f6855a205957
Parag Patil <parag.patil@thoughti.com>
2023-03-11 18:14:48 +0530

    improvements added in eslint configurations

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const port = 3030;
 
 // localhost:3030
 app.get('/', (req, res) => {
-  res.send('Hello world');
+    res.send('Hello world');
 });
 
 app.listen(port, () => {
-  console.log('app listening on port');
+    console.log('app listening on port');
 });
